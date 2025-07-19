de655e0e422959c7f84588b7b11dc8443359d250
Nikhil singh <nikhilsingh1696@gmail.com>
2025-07-19 13:28:24 +0530

    Add socket.io integration for real-time communication; update server setup and constants

--- src/server.js
+++ src/server.js
@@ -1,14 +1,38 @@
 import { app } from "./app.js";
 import dotenv from "dotenv"
+import http from "http";
+import { Server } from "socket.io";
+
 dotenv.config();
 
+const PORT = process.env.PORT || 8005
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.ORIGIN_CORS || "*",
+        credentials: true,
+    }
+});
+io.on("connection", (socket) => {
+    console.log("🔌 socket connected", socket.id);
+
+    socket.on("move", (data) => {
+        console.log("move detected: ", data);
+        socket.boardcast.emit("opponent move", data)
+    });
+    socket.on("disconnect", () => {
+        console.log("server disconnected: ", socket.id);
+
+    });
+});
+
 try {
-    const PORT = process.env.PORT || 8005 
     console.log("server is about to start");
-    app.listen(PORT , () => {
+    server.listen(PORT, () => {
         console.log(`🚀 Server running at http://localhost:${PORT}`);
     })
 } catch (error) {
-    console.log("error is connecting to server!!",error)
+    console.log("error is connecting to server!!", error)
 }
 
